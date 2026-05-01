345a82786137467cca80cddc3db0d08917dc02df
Him C. <himc.29z@gmail.com>
2026-05-02 02:35:49 +0800

    bug fix: make the python backend threaded so multiple downloads can happen simultaneously

--- server.py
+++ server.py
@@ -97,5 +97,6 @@ if __name__ == "__main__":
     app.run(
         host='0.0.0.0', 
         port=5000, 
-        debug=os.getenv("FLASK_DEBUG", "false").lower() == "true"
+        debug=os.getenv("FLASK_DEBUG", "false").lower() == "true",
+        threaded=True
     )
